80d24020a0c7ca35be9b38924736549f7cbdcf00
guiurm <guiurm@gmail.com>
2024-12-17 12:05:23 +0100

    build: creates actin and config rollup

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,22 +1,28 @@
 import typescript from "@rollup/plugin-typescript";
+import { dts } from "rollup-plugin-dts";
 
 export default [
     {
         input: "src/index.ts",
         output: {
-            dir: "dist/",
+            dir: "dist/mjs",
             format: "esm",
             entryFileNames: "index.mjs",
         },
-        plugins: [typescript({ tsconfig: "./tsconfig.mjs.json" })],
+        plugins: [typescript({ declaration: false, tsconfig: "./tsconfig.json" })],
     },
     {
-        input: "dist/index.mjs",
+        input: "dist/mjs/index.mjs",
         output: {
-            dir: "dist/",
+            dir: "dist/cjs",
             format: "cjs",
             entryFileNames: "index.cjs",
         },
-        plugins: [typescript({ tsconfig: "./tsconfig.cjs.json" })],
+        plugins: [typescript({ declaration: false, tsconfig: "./tsconfig.cjs.json" })],
+    },
+    {
+        input: "./src/index.ts",
+        output: [{ file: "dist/types/index.d.ts", format: "es" }],
+        plugins: [dts()],
     },
 ];
